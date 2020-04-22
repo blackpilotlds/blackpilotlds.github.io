34b862ea945a899238065b6ed1797772a9862595
blackpilotlds <ale15006@byui.edu>
2020-04-22 16:08:01 -0600

    final course portal home page

--- WDD-230-10-Web-Frontend-Development-I/js/lastUpdated.js
+++ WDD-230-10-Web-Frontend-Development-I/js/lastUpdated.js
@@ -3,3 +3,4 @@
 //     document.getElementById("demo").innerHTML = x;
 //   }
 document.write("Last Updated: " + document.lastModified +"");
+// http://www.web-source.net/web_design_tips/last_modified_date_web_page_script.htm#.XqCiafl7lEY
