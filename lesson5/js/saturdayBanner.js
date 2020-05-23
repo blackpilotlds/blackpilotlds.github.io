99f1a53bd683ff6e5e94d9faf00be48c8f676828
blackpilotlds <ale15006@byui.edu>
2020-05-23 15:41:26 -0600

    still the same icons

--- lesson5/js/saturdayBanner.js
+++ lesson5/js/saturdayBanner.js
@@ -1,7 +1,7 @@
 let d = new Date();
 
 const banner = document.getElementById("saturdayBanner");
-if (d.getDay() == 6) {/*5-friday*/
+if (d.getDay() == 5) {/*5-friday*/
 	banner.style.display = "block";
 } else {
 	banner.style.display = "none";
