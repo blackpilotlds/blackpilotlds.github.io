93fbb2c1a7358f32787834b6a952c32d94f6f89d
blackpilotlds <ale15006@byui.edu>
2020-06-17 06:49:19 -0600

    prophets.js adjusted

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,25 +10,19 @@ fetch(requestURL)
         for (let i = 0; i < prophets.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
-            let birthDate = document.createElement('p');
-            let birthPlace = document.createElement('p');
+            let bDate = document.createElement('p');
+            let bPlace = document.createElement('p');
             let image = document.createElement('img');
 
             h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-            card.appendChild(h2);
-
-            birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-
-            card.appendChild(birthDate);
-
-            birthPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-
-            card.appendChild(birthPlace);
-
+            bDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            bPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
             image.setAttribute('src', prophets[i].imageurl);
             image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
 
+            card.appendChild(h2);
+            card.appendChild(bDate);
+            card.appendChild(bPlace);
             card.appendChild(image);
 
             document.querySelector('div.cards').appendChild(card);
