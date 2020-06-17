dd71f99516adafd800aa745b45abd3025e24f17e
blackpilotlds <ale15006@byui.edu>
2020-06-17 06:51:55 -0600

    js format

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -14,18 +14,18 @@ fetch(requestURL)
             let bPlace = document.createElement('p');
             let image = document.createElement('img');
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            bDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-            bPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+                h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+                bDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+                bPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+                image.setAttribute('src', prophets[i].imageurl);
+                image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
 
-            card.appendChild(h2);
-            card.appendChild(bDate);
-            card.appendChild(bPlace);
-            card.appendChild(image);
+                    card.appendChild(h2);
+                    card.appendChild(bDate);
+                    card.appendChild(bPlace);
+                    card.appendChild(image);
 
-            document.querySelector('div.cards').appendChild(card);
+                        document.querySelector('div.cards').appendChild(card);
         }
     });
 
