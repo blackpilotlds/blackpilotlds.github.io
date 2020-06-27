825719642caac116dcfe51677099509be1b4d153
blackpilotlds <ale15006@byui.edu>
2020-06-27 12:20:54 -0600

    completed

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,26 +1,29 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=756a0fad857ea7b6497b499e83b10f9d&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=756a0fad857ea7b6497b499e83b10f9d&units=imperial';
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         // console.log(jsObject);
-        document.getElementById('aboutWeather').textContent = jsObject.list[0].weather[0].description;
-        // document.getElementById('currentTemp').textContent = jsObject.list[6].main.temp.toFixed(0);
-        // document.getElementById('HUM').textContent = jsObject.list[0].main.humidity;
-        // document.getElementById('speed').textContent = jsObject.list[0].wind.speed;
+        document.getElementById('aboutWeather').textContent = jsObject.list[0].weather[0].main;
+        document.getElementById('currentTemp').textContent = jsObject.list[0].main.temp;
+        document.getElementById('HUM').textContent = jsObject.list[0].main.humidity;
+        document.getElementById('speed').textContent = jsObject.list[0].wind.speed;
 
-        // const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
-        // const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        const fiveDayForecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
 
-        // let days = 0;
-        // forecast.forEach(fiveDayForecast => {
-        //     let d = new Date(fiveDayForecast.dt_txt);
-        //     document.getElementById(`temp${i + 1}`).innerHTML = `${fiveDayForecast.main.temp.toFixed(0)}&deg;F`;
-        //     document.getElementById(`day${i + 1}`).textContent = week[d.getDay()];
-        //     const imagesrc = `https://openweathermap.org/img/wn/${fiveDayForecast.weather[0].icon}@2x.png`;
-        //     const desc = fiveDayForecast.weather[0].description;
-        //     document.getElementById(`icon${i + 1}`).setAttribute('src', imagesrc);
-        //     document.getElementById(`icon${i + 1}`).setAttribute('alt', desc);
-        //     days++;
-        // });
+        console.log(fiveDayForecast);
+
+        const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        let days = 0;
+        fiveDayForecast.forEach(forecast => {
+            let d = new Date(forecast.dt_txt);
+            document.getElementById(`temp${days+ 1}`).innerHTML = `${forecast.main.temp.toFixed(0)}`;
+            document.getElementById(`day${days+ 1}`).textContent = week[d.getDay()];
+            const imagesrc = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+            const desc = forecast.weather[0].description;
+            document.getElementById(`icon${days+ 1}`).setAttribute('src', imagesrc);
+            document.getElementById(`icon${days+ 1}`).setAttribute('alt', desc);
+            days++;
+        });
 
     });
