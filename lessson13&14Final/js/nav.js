ea6775d8940ac4f719a9672e0721cec36cdd72ef
blackpilotlds <ale15006@byui.edu>
2020-07-21 07:22:36 -0600

    weather summary added

--- lessson13&14Final/js/nav.js
+++ lessson13&14Final/js/nav.js
@@ -1,8 +1,14 @@
+/* #region  NAVIGATION BAR */
+
 // navigation
 const hambutton = document.querySelector('.hamburger');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false); // end for navigation
+hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false); // end for navigation
+
+/* #endregion */
+
+/* #region  SLIDESHOW */
 
 // for slideshow
 var slideIndex = 1;
@@ -15,12 +21,22 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex - 1].style.display = "block";
 } // end for slideshow
 
-// [BOOKMARK] https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
+/* #endregion */
+
+const apiUrlWeather = 'https://api.openweathermap.org/data/2.5/weather?id=3530103&appid=756a0fad857ea7b6497b499e83b10f9d&units=imperial';
+fetch(apiUrlWeather)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        // console.log(jsObject);
+        document.getElementById('aboutWeather').textContent = jsObject.weather[0].main;
+        document.getElementById('currentTemp').textContent = jsObject.main.temp;
+        document.getElementById('HUM').textContent = jsObject.main.humidity;
+    }); 
