a9313028310c1ea862b4a789806cc7f2f3010a09
blackpilotlds <ale15006@byui.edu>
2020-07-22 07:17:48 -0600

    almost done with the template of my first page

--- lessson13&14FinalV0.1/js/nav.js
+++ lessson13&14FinalV0.1/js/nav.js
@@ -21,6 +21,30 @@ fetch(apiUrlWeather)
         document.getElementById('HUM').textContent = jsObject.main.humidity;
     }); // ENDS WEATHER
 
+// FIVE DAY FORECAST STARTS
+const apiUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?id=3530103&appid=756a0fad857ea7b6497b499e83b10f9d&units=imperial';
+fetch(apiUrlForecast)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        // console.log(jsObject);
+
+            const fiveDayForecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+            // console.log(fiveDayForecast);
+            const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+                let days = 0;
+                    fiveDayForecast.forEach(forecast => {
+                        let d = new Date(forecast.dt_txt);
+                        let imgage = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+                        let desc = forecast.weather[0].description;
+                            document.getElementById(`temp${days+ 1}`).innerHTML = `${forecast.main.temp.toFixed(0)}`;
+                            document.getElementById(`day${days+ 1}`).textContent = week[d.getDay()];
+                            document.getElementById(`icon${days+ 1}`).setAttribute('src', imgage);
+                            document.getElementById(`icon${days+ 1}`).setAttribute('alt', desc);
+                days++;
+                });
+    }); // FIVE DAY FORECAST ENDS
+
 /* #endregion */
 
 /* #region  LAZY LOAD */
@@ -51,6 +75,22 @@ else {
     imagesToLoad.forEach(function (img) {
         loadImages(img);
     });
+}
+
+/* #endregion */
+
+/* #region  LAST TIME UPDATED DATE */
+
+document.getElementById("lastUpdated").innerHTML = formatAMPM();
+
+function formatAMPM() {
+    var d = new Date(),
+        minutes = d.getMinutes().toString().length == 1 ? '0' + d.getMinutes() : d.getMinutes(),
+        hours = d.getHours().toString().length == 1 ? '0' + d.getHours() : d.getHours(),
+        ampm = d.getHours() >= 12 ? 'pm' : 'am',
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[d.getDay()] + ', ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear(); /*+' '+hours+':'+minutes+ampm;*/
 }
 
 /* #endregion */
